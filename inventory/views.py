684704570616da7e9e79a5f507a418571b665cf0
JayAutus <jacobautus2@gmail.com>
2026-02-19 10:49:05 -0800

    Implement menu item and recipe management features
    - Added update and delete views for MenuItem and RecipeRequirement.
    - Enhanced menu item form to support editing and improved user feedback with success messages.
    - Updated URLs to include new routes for editing and managing recipes.
    - Modified templates to reflect dynamic titles and improved navigation links for better user experience.

--- inventory/views.py
+++ inventory/views.py
@@ -2,7 +2,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, CreateView, UpdateView
+from django.contrib import messages
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.urls import reverse, reverse_lazy
 from django.db.models import Sum, F
 from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
 from .forms import IngredientForm, MenuItemForm, RecipeRequirementForm, PurchaseForm
@@ -71,17 +73,82 @@ class MenuItemCreateView(LoginRequiredMixin, CreateView):
     model = MenuItem
     form_class = MenuItemForm
     template_name = 'inventory/menuitem_form.html'
+    success_url = reverse_lazy('inventory:menuitem_list')
+
+
+class MenuItemUpdateView(LoginRequiredMixin, UpdateView):
+    model = MenuItem
+    form_class = MenuItemForm
+    template_name = 'inventory/menuitem_form.html'
+    success_url = reverse_lazy('inventory:menuitem_list')
+    context_object_name = 'menuitem'
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Menu item updated successfully.')
+        return super().form_valid(form)
+
+
+class MenuItemRecipeView(LoginRequiredMixin, DetailView):
+    """List recipe requirements for a menu item; edit/delete/add ingredients."""
+    model = MenuItem
+    template_name = 'inventory/menuitem_recipe.html'
+    context_object_name = 'menuitem'
 
 
 class RecipeRequirementCreateView(LoginRequiredMixin, CreateView):
     model = RecipeRequirement
     form_class = RecipeRequirementForm
     template_name = 'inventory/reciperequirement_form.html'
+    success_url = reverse_lazy('inventory:recipe_create')
+    
+    def get_initial(self):
+        initial = super().get_initial()
+        menu_item_pk = self.request.GET.get('menu_item')
+        if menu_item_pk and MenuItem.objects.filter(pk=menu_item_pk).exists():
+            initial['menu_item'] = MenuItem.objects.get(pk=menu_item_pk)
+        return initial
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['menu_items'] = MenuItem.objects.all()
         return context
+    
+    def get_success_url(self):
+        menu_item_pk = self.request.GET.get('menu_item')
+        if menu_item_pk:
+            return reverse('inventory:menuitem_recipe', kwargs={'pk': menu_item_pk})
+        return super().get_success_url()
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Recipe requirement was added successfully.')
+        return super().form_valid(form)
+
+
+class RecipeRequirementUpdateView(LoginRequiredMixin, UpdateView):
+    model = RecipeRequirement
+    form_class = RecipeRequirementForm
+    template_name = 'inventory/reciperequirement_form.html'
+    context_object_name = 'requirement'
+
+    def get_success_url(self):
+        return reverse('inventory:menuitem_recipe', kwargs={'pk': self.object.menu_item_id})
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Recipe requirement updated successfully.')
+        return super().form_valid(form)
+
+
+class RecipeRequirementDeleteView(LoginRequiredMixin, DeleteView):
+    model = RecipeRequirement
+    context_object_name = 'requirement'
+    template_name = 'inventory/reciperequirement_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse('inventory:menuitem_recipe', kwargs={'pk': self.object.menu_item_id})
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Recipe requirement removed.')
+        return super().form_valid(form)
 
 
 class PurchaseListView(LoginRequiredMixin, ListView):
